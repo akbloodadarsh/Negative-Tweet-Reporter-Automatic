e59106cd6ce4c9b97f905d259054203626d0e7c4
Adarsh Kunwar <adarsh.infinite@gmail.com>
2019-12-17 19:09:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -52,63 +52,72 @@ def process(data):
     return data['sentence']
 
 
-def execute(hashtag):
-    try:
-        num_tweets = 10
-        twitter_client = TwitterClient()
-        api = twitter_client.get_twitter_client_api()
-        tweets = Cursor(api.search, q=hashtag, rpp=100).items(num_tweets)
-        datafile = pd.read_csv('Train.csv', sep=',', encoding="utf-8")
-        x = process(datafile)
-        y = datafile['label']
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
-        vector = CountVectorizer()
-        vector.fit(x_train)
-        x_train_vft = vector.transform(x_train)
-        x_test_vft = vector.transform(x_test)
-        for tweet in tweets:
-            if tweet.lang == 'en':
-                text = [pp.pre_processing(tweet.text)]
-                vec = vector.transform(text)
-                temp = mnb.MultinomialNBAlgo(x_train_vft, y_train, x_test_vft, y_test, vec)
-                if temp == 'Negative':
-                    print('Negative Tweet Encountered')
-                    user = tweet.entities.get('user_mentions')
-                    name = str((user[0].get('screen_name')))
-                    print('Reporting..')
-                    api.report_spam(screen_name=name)
-                for i in range(2):
-                    print('....')
-                    time.sleep(1)
-
-    except Exception as e:
-        # Print the error
-        print(e)
-
-        # When reach the rate limit
-        def on_limit(self, track):
-            # Print rate limiting error
-            print("Rate limited, continuing")
-            # Continue mining tweets
-            return True
-        # When timed out
-
-        def on_timeout(self):
-            # Print timeout message
-            print(sys.stderr, 'Timeout')
-            # Wait 10 seconds
-            time.sleep(10)
-            # Return nothing
-            return True
-
-
-if __name__ == '__main__':
+def execute():
     TotalTweets = 0
-    print('Enter the hashtag you want to check and report\n')
-    hashtag = str(input())
+    hashtag = Entry1.get()
     while TotalTweets != 3200:
         print('fetching tweets please wait\n')
         time.sleep(3)
-        execute(hashtag)
+        try:
+            num_tweets = 10
+            twitter_client = TwitterClient()
+            api = twitter_client.get_twitter_client_api()
+            tweets = Cursor(api.search, q=hashtag, rpp=100).items(num_tweets)
+            datafile = pd.read_csv('Train.csv', sep=',', encoding="utf-8")
+            x = process(datafile)
+            y = datafile['label']
+            x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+            vector = CountVectorizer()
+            vector.fit(x_train)
+            x_train_vft = vector.transform(x_train)
+            x_test_vft = vector.transform(x_test)
+            for tweet in tweets:
+                if tweet.lang == 'en':
+                    text = [pp.pre_processing(tweet.text)]
+                    vec = vector.transform(text)
+                    temp = mnb.MultinomialNBAlgo(x_train_vft, y_train, x_test_vft, y_test, vec)
+                    if temp == 'Negative':
+                        print('Negative Tweet Encountered')
+                        user = tweet.entities.get('user_mentions')
+                        name = str((user[0].get('screen_name')))
+                        print('Reporting..')
+                        api.report_spam(screen_name=name)
+                    for i in range(2):
+                        print('....')
+                        time.sleep(1)
+
+        except Exception as e:
+            # Print the error
+            print(e)
+
+            # When reach the rate limit
+            def on_limit(self, track):
+                # Print rate limiting error
+                print("Rate limited, continuing")
+                # Continue mining tweets
+                return True
+            # When timed out
+
+            def on_timeout(self):
+                # Print timeout message
+                print(sys.stderr, 'Timeout')
+                # Wait 10 seconds
+                time.sleep(10)
+                # Return nothing
+                return True
         TotalTweets += 10
         print('Total Tweets Analysed = ', TotalTweets)
+
+
+if __name__ == '__main__':
+    mainwindow = Tk()
+    mainwindow.title("Twitter Sentimental Analysis Engine")
+    Label(mainwindow, text="TWITTER SENTIMENTAL ANALYSIS ENGINE", bg="black", fg="white").pack(side=TOP, fill=X, padx=2, pady=2)
+    photo = PhotoImage(file="Twitterlogo.png")
+    Label(mainwindow, image=photo, bg="black", fg="white").pack(side=TOP, fill=X)
+    Label(mainwindow, text="HASHTAG", bg="black", fg="white").pack(side=TOP, fill=X, padx=2, pady=2)
+    Entry1 = Entry(mainwindow)
+    Entry1.pack(side=TOP, padx=2, pady=2)
+    But1 = Button(mainwindow, text="REPORT", command=execute)
+    But1.pack(side=TOP, fill=X, padx=2, pady=2)
+    mainwindow.mainloop()
